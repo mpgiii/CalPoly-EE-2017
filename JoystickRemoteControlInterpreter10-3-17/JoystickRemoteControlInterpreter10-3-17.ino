1afa1b9d6b99d6a5023be181851eb405af58f3ca
Michael Georgariou III <mpgiii@ymail.com>
2017-10-01 17:48:19 -0700

    2.0

--- JoystickRemoteControlInterpreter10-3-17/JoystickRemoteControlInterpreter10-3-17.ino
+++ JoystickRemoteControlInterpreter10-3-17/JoystickRemoteControlInterpreter10-3-17.ino
@@ -1,9 +1,41 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ * Michael Georgariou
+ * Joystick Remote Control Interpretter
+ * EE 151-03
+ */
 
+int Ain0 = 0;
+int Ain1 = 0;
+float Volt0Reading;
+float Volt1Reading;
+float Joy0Pos;
+float Joy1Pos;
+
+void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Ain0 = analogRead(0);
+  Ain1 = analogRead(1);
+  Volt0Reading = (Ain0 / 204.0);
+  Volt1Reading = (Ain1 / 204.0);
+  Joy0Pos = ((Volt0Reading * 40) - 100);
+  Joy1Pos = ((Volt1Reading * 40) - 100);
 
+  Serial.print("Joy0 Analog Value: ");
+  Serial.println(Ain0);
+  Serial.print("Joy0 Volt Reading: ");
+  Serial.println(Volt0Reading);
+  Serial.print("Joy0 Position: ");
+  Serial.print(Joy0Pos);
+  Serial.println(" ");
+  
+  Serial.print("Joy1 Analog Value: ");
+  Serial.println(Ain1);
+  Serial.print("Joy1 Volt Reading: ");
+  Serial.println(Volt1Reading);
+  Serial.print("Joy1 Position: ");
+  Serial.print(Joy1Pos);
+  Serial.println(" ");
 }
