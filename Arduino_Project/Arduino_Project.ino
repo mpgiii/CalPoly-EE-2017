e389d55644d73839c047617bcecf401b0c49f3e4
Michael Georgariou III <mpgiii@ymail.com>
2017-10-30 16:19:35 -0700

    added LED status lights

--- Arduino_Project/Arduino_Project.ino
+++ Arduino_Project/Arduino_Project.ino
@@ -1,6 +1,9 @@
 #include <Keypad.h>
 
 int Speaker = 13;
+int d; //to store if the button has been pressed to switch between major and minor
+int YellowLED = 18;
+int RedLED = 17;
 
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
@@ -21,11 +24,30 @@ void setup(){
   
   Serial.begin(9600);
   pinMode(Speaker, OUTPUT);
+  pinMode(12, INPUT);
+  pinMode(RedLED, OUTPUT);
+  pinMode(YellowLED, OUTPUT);
   keypad.setHoldTime(999999999999999999999999999999999999999);    //hack way of avoiding hold function, not working for me
 }
   
 void loop(){
-  char key = keypad.getKey();           //find which button is being pressed
+  d = digitalRead(12);
+  Serial.println(d);
+  if (d == LOW) {
+    Major();
+    digitalWrite(RedLED, LOW);
+    digitalWrite(YellowLED, HIGH);      //Light up Yellow LED and play on a major scale
+  }
+  else {
+    Minor();
+    Serial.println("PRESSED");
+    digitalWrite(RedLED, HIGH);
+    digitalWrite(YellowLED, LOW);       //Light up Red LED and play on a minor scale
+  }
+}
+
+void Major() {
+    char key = keypad.getKey();         //find which button is being pressed
 
   if (keypad.getState() == PRESSED) {   //while pressed:
       switch(key) {
@@ -77,9 +99,70 @@ void loop(){
         case 'G':
           tone(Speaker, 131);
       }
-      Serial.println("Held");                 //debug in Serial
+//      Serial.println("Held");          //debug in Serial
   }
-  else {                                      //when nothing is pressed, stop the tone
+  else {                                 //when nothing is pressed, stop the tone
     noTone(Speaker);
   }
 }
+
+void Minor() {
+    char key = keypad.getKey();         //find which button is being pressed
+
+  if (keypad.getState() == PRESSED) {   //while pressed:
+      switch(key) {
+        case '1':                       //if key 1 is pressed, play a middle C
+          tone(Speaker, 261);
+          break;
+        case '2':                       // if key 2 is pressed, play a middle D... etc etc
+          tone(Speaker, 294);
+          break;
+        case '3':
+          tone(Speaker, 311);
+          break;
+        case '4':
+          tone(Speaker, 349);
+          break;
+        case '5':
+          tone(Speaker, 392);
+          break;
+        case '6':
+          tone(Speaker, 440);
+          break;
+        case '7':
+          tone(Speaker, 466);
+          break;
+        case '8':
+          tone(Speaker, 523);
+          break;
+        case '9':
+          tone(Speaker, 587);
+          break;
+        case 'A':
+          tone(Speaker, 622);
+          break;
+        case 'B':
+          tone(Speaker, 698);
+          break;
+        case 'C':
+          tone(Speaker, 784);
+          break;
+        case 'D':
+          tone(Speaker, 880);
+          break;
+        case 'E':
+          tone(Speaker, 932);
+          break;
+        case 'F':
+          tone(Speaker, 1046);
+          break;
+        case 'G':
+          tone(Speaker, 131);
+      }
+//      Serial.println("Held");              //debug in Serial
+  }
+  else {                                     //when nothing is pressed, stop the tone
+    noTone(Speaker);
+  }
+}
+
